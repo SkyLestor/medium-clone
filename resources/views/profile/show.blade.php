c77d217a53c73c45999cb744c82c26f46d0ab090
SkyLestor <timur_skocylov@outlook.com>
2026-01-06 18:30:02 +0200

    Profile page fronted bug fix

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -2,7 +2,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <fol class="flex">
+                <div class="flex">
                     <div class="flex-1">
                         <div>
                             <h1 class="text-5xl dark:text-white">
@@ -40,8 +40,8 @@
                             </div>
                         @endif
                     </x-follow-ctr>
+                </div>
             </div>
         </div>
-    </div>
     </div>
 </x-app-layout>
