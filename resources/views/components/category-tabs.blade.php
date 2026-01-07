ccb070e93d0f809bab5955a388ce36e8386c74ea
SkyLestor <timur_skocylov@outlook.com>
2026-01-07 18:59:10 +0200

    Category filtering implemented

--- resources/views/components/category-tabs.blade.php
+++ resources/views/components/category-tabs.blade.php
@@ -1,15 +1,22 @@
 <div class="p-6 text-gray-900 dark:text-gray-100">
     <ul class="flex -mb-px space-x-8 justify-center">
         <li>
-            <a href="#"
-               class="inline-block py-4 px-1 border-b-2 border-indigo-500 text-indigo-600 dark:text-indigo-400 font-medium text-sm">
+            <a href="{{ route('dashboard') }}"
+               class=" {{ !request('category')
+                ? 'inline-block py-4 px-1 border-b-2 border-indigo-500 text-indigo-600 dark:text-indigo-400 font-medium text-sm'
+                : 'inline-block py-4 px-1 border-b-2 border-transparent text-gray-500 dark:text-gray-400
+                hover:text-gray-700 dark:hover:text-gray-200 hover:border-gray-300 transition-all duration-200' }}">
                 All
             </a>
         </li>
         @foreach($categories as $category)
             <li>
-                <a href="#"
-                   class="inline-block py-4 px-1 border-b-2 border-transparent text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200 hover:border-gray-300 transition-all duration-200">
+                <a href="{{ route('post.byCategory', $category) }}"
+                   class="{{ Route::currentRouteNamed('post.byCategory') && request('category')->id == $category->id
+                ? 'inline-block py-4 px-1 border-b-2 border-indigo-500 text-indigo-600 dark:text-indigo-400 font-medium
+                text-sm'
+                : 'inline-block py-4 px-1 border-b-2 border-transparent text-gray-500 dark:text-gray-400
+                hover:text-gray-700 dark:hover:text-gray-200 hover:border-gray-300 transition-all duration-200' }}">
                     {{$category->name}}
                 </a>
             </li>
