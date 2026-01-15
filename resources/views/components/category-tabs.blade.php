ac14504f90b53f410151f0806fcd98b1f29fa230
SkyLestor <timur_skocylov@outlook.com>
2026-01-15 10:47:12 +0200

    Added responsive version to the category-tabs.blade.php

--- resources/views/components/category-tabs.blade.php
+++ resources/views/components/category-tabs.blade.php
@@ -1,11 +1,11 @@
 <div class="p-6 text-gray-900 dark:text-gray-100">
-    <ul class="flex -mb-px space-x-8 justify-center">
+    <ul class="flex gap-6 overflow-x-auto whitespace-nowrap px-4 sm:justify-center sm:overflow-visible scrollbar-hide">
         <li>
             <a href="{{ route('dashboard') }}"
-               class=" {{ !request('category')
-                ? 'inline-block py-4 px-1 border-b-2 border-indigo-500 text-indigo-600 dark:text-indigo-400 font-medium text-sm'
-                : 'inline-block py-4 px-1 border-b-2 border-transparent text-gray-500 dark:text-gray-400
-                hover:text-gray-700 dark:hover:text-gray-200 hover:border-gray-300 transition-all duration-200' }}">
+               class="{{ !request('category')
+                ? 'inline-block py-3 px-2 border-b-2 border-indigo-500 text-indigo-600 dark:text-indigo-400 font-medium text-sm'
+                : 'inline-block py-3 px-2 border-b-2 border-transparent text-gray-500 dark:text-gray-400
+                   hover:text-gray-700 dark:hover:text-gray-200 hover:border-gray-300 transition-all duration-200' }}">
                 @if(auth()->user())
                     Follows
                 @else
@@ -13,15 +13,15 @@
                 @endif
             </a>
         </li>
+
         @foreach($categories as $category)
             <li>
                 <a href="{{ route('post.byCategory', $category) }}"
                    class="{{ Route::currentRouteNamed('post.byCategory') && request('category')->id == $category->id
-                ? 'inline-block py-4 px-1 border-b-2 border-indigo-500 text-indigo-600 dark:text-indigo-400 font-medium
-                text-sm'
-                : 'inline-block py-4 px-1 border-b-2 border-transparent text-gray-500 dark:text-gray-400
-                hover:text-gray-700 dark:hover:text-gray-200 hover:border-gray-300 transition-all duration-200' }}">
-                    {{$category->name}}
+                    ? 'inline-block py-3 px-2 border-b-2 border-indigo-500 text-indigo-600 dark:text-indigo-400 font-medium text-sm'
+                    : 'inline-block py-3 px-2 border-b-2 border-transparent text-gray-500 dark:text-gray-400
+                       hover:text-gray-700 dark:hover:text-gray-200 hover:border-gray-300 transition-all duration-200' }}">
+                    {{ $category->name }}
                 </a>
             </li>
         @endforeach
